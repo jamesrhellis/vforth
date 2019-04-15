73afa357c2a25c33743f13dcd5e4d50948146ac6
james <james@jgnuvoid.default>
2019-04-15 19:12:51 +0100

    Remove supplimentary stack
    This was included to avoid fat pointers on the stack, as it would
    make stack manipulations harder - however the two item stack manipulations
    are easy enough without the stack, and other manipulations can be done
    in combination with the return stack.
    
    This means that the simplified vm will be hardware compatiable with
    a larger range of hardware, including existing stack cpus.
    
    This should also simplify a hardware implimentation.

--- interpreter.c
+++ interpreter.c
@@ -28,22 +28,15 @@ enum ins {
 	I_DUP,
 	I_EDUP,
 	I_ROT,
-	I_EROT,
 	I_NROT,
-	I_ENROT,
 
 	I_TR,
-	I_ETR,
 	I_FR,
-	I_EFR,
 
 	// Data
 	I_ZERO,
-	I_EZERO,
 	I_IMM8,
-	I_EIMM8,
 	I_IMM32,
-	I_EIMM32,
 
 	I_LD,
 	I_ELD,
@@ -64,9 +57,7 @@ enum ins {
 
 	// Branching
 	I_BLT,
-	I_EBLT,
 	I_BGT,
-	I_EBGT,
 	I_BZ,
 	I_BNZ,
 
@@ -76,12 +67,14 @@ enum ins {
 	NO_I,
 };
 
-typedef void (* syscall)(ins **pc, stack *s, stack *ret, stack *sup);
+#define STATE ins **pc, stack *s, stack *ret
 
-void exit(ins **pc, stack *s, stack *ret, stack *sup){
+typedef void (* syscall)(STATE);
+
+void exit(STATE){
 	*pc = NULL;
 }
-void test(ins **pc, stack *s, stack *ret, stack *sup){
+void test(STATE){
 	printf("%d", s->top);
 }
 
@@ -97,8 +90,7 @@ int main(int argn, char ** args) {
 	
 	stack s = {0};
 	stack ret = {0};
-	stack sup = {0};
-	reg tmp = 0;
+	reg tmp = 0, tmp2 = 0;
 	ins *pc = (ins []){I_IMM8, 12, I_IMM8, 13, I_ADD, I_SYS, 1, 0, I_SYS, 0, 0};
 	while (pc) {
 		switch (*(pc++)) {
@@ -107,7 +99,7 @@ int main(int argn, char ** args) {
 			memcpy(&tmp, pc, 2);
 			pc += 2;
 
-			syscalls[tmp](&pc, &s, &ret, &sup);
+			syscalls[tmp](&pc, &s, &ret);
 
 			break;
 		case I_ADD:
@@ -124,9 +116,13 @@ int main(int argn, char ** args) {
 			break;
 
 		case I_ESWAP:
-			tmp = sup.top;
-			sup.top = sup.items[sup.size - 1];
-			sup.items[sup.size - 1] = tmp;
+			tmp = s.top;
+			tmp2 = s.items[s.size -  1];
+			s.top = s.items[s.size - 2];
+			s.items[s.size - 1] = s.items[s.size - 3];
+			s.items[s.size - 2] = tmp;
+			s.items[s.size - 3] = tmp2;
+			break;
 		case I_SWAP:
 			tmp = s.top;
 			s.top = s.items[s.size - 1];
@@ -134,24 +130,24 @@ int main(int argn, char ** args) {
 			break;
 
 		case I_EOVER:
-			sup.items[sup.size++] = sup.top;
-			sup.top = sup.items[sup.size - 2];
+			s.items[s.size] = s.top;
+			s.items[s.size + 1] = s.items[s.size - 3];
+			s.top = s.items[s.size - 2];
+			s.size += 2;
+			break;
 		case I_OVER:
 			s.items[s.size++] = s.top;
 			s.top = s.items[s.size - 2];
 			break;
 
 		case I_EDUP:
-			sup.items[sup.size++] = sup.top;
+			s.items[s.size++] = s.top;
+			s.items[s.size] = s.items[s.size - 2];
+			break;
 		case I_DUP:
 			s.items[s.size++] = s.top;
 			break;
 
-		case I_EROT:
-			tmp = sup.items[sup.size - 2];
-			sup.items[sup.size - 2] = sup.items[sup.size - 1];
-			sup.items[sup.size - 1] = sup.top;
-			sup.top = tmp;
 		case I_ROT:
 			tmp = s.items[s.size - 2];
 			s.items[s.size - 2] = s.items[s.size - 1];
@@ -159,12 +155,6 @@ int main(int argn, char ** args) {
 			s.top = tmp;
 			break;
 
-		case I_ENROT:
-			tmp = sup.top;
-			sup.top = sup.items[sup.size - 1];
-			sup.items[sup.size - 1] = sup.items[sup.size - 2];
-			sup.items[sup.size - 2] = tmp;
-			break;
 		case I_NROT:
 			tmp = s.top;
 			s.top = s.items[s.size - 1];
@@ -172,24 +162,10 @@ int main(int argn, char ** args) {
 			s.items[s.size - 2] = tmp;
 			break;
 
-		case I_ETR:
-			ret.items[ret.size++] = ret.top;
-			ret.top = sup.top;
-			sup.top = sup.items[--sup.size];
 		case I_TR:
 			ret.items[ret.size++] = ret.top;
 			ret.top = s.top;
 			s.top = s.items[--s.size];
-			break;
-		case I_EFR:
-			s.items[s.size++] = s.top;
-			s.top = ret.top;
-			ret.top = ret.items[--ret.size];
-
-			sup.items[sup.size++] = sup.top;
-			sup.top = ret.top;
-			ret.top = ret.items[--ret.size];
-
 			break;
 		case I_FR:
 			s.items[s.size++] = s.top;
@@ -201,17 +177,9 @@ int main(int argn, char ** args) {
 			s.items[s.size++] = s.top;
 			s.top = 0;
 			break;
-		case I_EZERO:
-			sup.items[sup.size++] = sup.top;
-			sup.top = 0;
-			break;
 		case I_IMM8:
 			s.items[s.size++] = s.top;
 			s.top = *(pc++);
-			break;
-		case I_EIMM8:
-			sup.items[sup.size++] = sup.top;
-			sup.top = *(pc++);
 			break;
 		case I_IMM32:
 			s.items[s.size++] = s.top;
@@ -219,22 +187,14 @@ int main(int argn, char ** args) {
 			memcpy(&s.top, pc, 4);
 			pc += 4;
 			break;
-		case I_EIMM32:
-			sup.items[sup.size++] = sup.top;
-			sup.top = 0;
-			memcpy(&sup.top, pc, 4);
-			pc += 4;
-			break;
 
 		case I_ELD:
-			sup.items[sup.size++] = sup.top;
-			sup.top = *((reg *)s.top +  1);
+			s.items[s.size++] = *((reg *)s.top + 1);
 		case I_LD:
 			s.top = *(reg *)s.top;
 			break;
 		case I_EST:
-			*((reg *)s.top + 1) = sup.top;
-			sup.top = sup.items[--sup.size];
+			*((reg *)s.top + 1) = s.items[s.size-2];
 		case I_ST:
 			*(reg *)s.top = s.items[--s.size];
 			break;
@@ -273,20 +233,8 @@ int main(int argn, char ** args) {
 		#define SKIP {pc += 3;}
 		#define BRANCH {memcpy(&tmp, pc, 3); tmp |= tmp & 0x800000 ? ~(reg)0xFFFFFF : 0; pc += tmp;}
 
-		case I_EBGT:
-			if (s.top > sup.top)
-				BRANCH
-			else
-				SKIP
-			break;
 		case I_BGT:
 			if (s.items[s.size - 1] > s.top)
-				BRANCH
-			else
-				SKIP
-			break;
-		case I_EBLT:
-			if (s.top < sup.top)
 				BRANCH
 			else
 				SKIP
