c598bb541f06c07f2dda39eeb5afa1766c896bc2
James R H Ellis <jamesrhellis@gmail.com>
2019-04-24 19:41:06 +0100

    Remove unimplimented dictionary searching words

--- test.f
+++ test.f
@@ -1,8 +1,4 @@
 in base.f
-( Impliment the higher level forth in forth )
-: find-word ( word-def word-start end  -- word-ref ) dup >r 1 w + @ ;
-: find-word ( dict word-start end  -- word-ref ) @ find-word ;
-
 : test 23 24 + ;
 
 : loop-test dup 0 = if drop exit then . 1 - tail ;
