893410fa5dc5f2a5f60edd7af9d658b45f633d47
James R H Ellis <jamesrhellis@gmail.com>
2019-05-06 16:33:52 +0100

    New ownership checking logic
    Now accounts for fuctions claiming ownership of args by adding them to the bitmap during the search.
    Fix use of case to match the new semantics from master

--- lang.f
+++ lang.f
@@ -35,9 +35,9 @@ in base.f
 : >name name ! ;
 : args 3 w + ;	( Types of arguements - list )
 : ret 3 w + 1 + ;	( Types of return items )
+: rel-claim 3 w + 2 + ;	( Ownerships from stack claimed - 8 bit bitmap )
 : types 4 w + ; ( Combined storage of return and call types )
 : abs-claim 12 w + ;	( Global ownerships claimed - bitset )
-: rel-claim 13 w + ;	( Ownerships from stack claimed - 8 bit list )
 : height 14 w + ;	( Max height increase of stacks - 8 bit list )
 			( 0 - main , 1 - ret , 2/3 reserved for fp / vec ) 
 : height-main 14 w + ; 
@@ -45,7 +45,7 @@ in base.f
 : code 15 w + ; 	( Code - bounded pointer )
 
 0 var dict !
-: find ( word? name -- word? ) 0 case swap drop exit then
+: find ( word? name -- word? ) 0 case drop 0 exit then
 	dup >r name> string-eq if 2drop r> exit then
 	drop r> next> tail ;
 
@@ -65,6 +65,7 @@ in base.f
 	over ts-push r> swap ts-push ;
 : ts-dup ( stack ) dup items over top @ w + @ swap ts-push ;
 : ts-over ( stack ) dup top @ 1 - w over items @ swap ts-push ;
+: ts-pick ( n stack -- item ) over top swap - w swap items + @;
 : items 1 w + ;
 
 ( Type manipulation words )
@@ -75,6 +76,7 @@ in base.f
 	13-14 ref-type : 0 -> not a ref, 1 -> ref to stack item,
 		2 -> ref to global item, 3 -> ownership
 	15-20 ref? ; what object is this a ref of / 0 -> own > ref
+	21 mark	; used for marking value in compiler
 )
 	
 0 invert 10 lshift invert con type-mask
@@ -90,6 +92,8 @@ in base.f
 : >bounded ( to own -- to ) bounded-mask invert and swap bounded-mask and or ;
 
 0 invert 2 lshift invert con ref-type-mask
+2 con ref-abs
+3 con ref-rel
 : ref-type 13 rshift ref-type-mask and ;
 : >ref-type ( to type -- to ) ref-type-mask 13 lshift invert and swap
 	ref-type-mask and 13 lshift or ;
@@ -114,12 +118,20 @@ in base.f
 ( Type checking )
 
 : set-bit ( bit -- bitmap ) 1 swap lshift ;
-: list-ownership-bitmap ( n bitmap list -- bitmap ) 
-	. 0 case drop swap drop exit then 1 - >r >r
-	dup 1 w + swap @ dup ref-type 
-	2 != if drop r> r> tail then
-	ref set-bit . r> . or r> tail ;
-: list-ownership-bitmap ( list n -- bitmap ) swap 0 swap list-ownership-bitmap ;
-: stack-ownership-bitmap ( stack n -- bitmap ) items list-ownership-bitmap ;
 
-: check-ownership ( stack word ) stack-ownership-bitmap swap abs-claim and ;
+: check-abs-ownership ( stack bitmap -- bitmap ) dup top swap items stack-ownership-bitmap and ;
+
+: check-add-ownership ( item add-bit? abs-bitmap rel-bitmap -- conflict? abs-bitmap rel-bitmap )
+	swap >r dup ref set-bit swap ref-type
+	ref-abs case over or r>
+		if swap then over xor 0 != exit then
+	ref-rel case swap rot over or r>
+		if swap then over xor 0 != >r swap r> then
+	drop drop 0 exit ;
+: check-list-ownership ( n arg-ref-bitmap list abs-bitmap rel-bitmap -- work-or-fail )
+	0 case ( SUCCESS ) 2drop 2drop false exit then >r ( check end of list )
+	dup 1 rshift >r 1 and 0 !=		( check if ownership taken )
+	swap dup 1 w - >r @		( get item )
+	check-add-ownership if ( FAIL ) r> r> r> drop 2drop 2drop true exit then
+	r> r> r> tail ;
+	
